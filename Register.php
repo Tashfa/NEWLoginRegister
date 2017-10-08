c0d404a1d12c3ad599ae1081165e7732e54c22d7
Tashfa <32612197+Tashfa@users.noreply.github.com>
2017-10-08 04:07:34 -0700

    MyRegister.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id3186240_tashfa", "Paradise123", "id3186240_mydatabase");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
